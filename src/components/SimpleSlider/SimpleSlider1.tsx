0f60bbcddf910061acbca29b21bae396e1344d47
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-08-04 11:45:09 -0300

    gdrive image 1

--- src/components/SimpleSlider/SimpleSlider1.tsx
+++ src/components/SimpleSlider/SimpleSlider1.tsx
@@ -29,7 +29,7 @@ export default function SimpleSlider1() {
   }
   return (
     <Slider {...settings}>
-      <Images url={Img1} alt="" />
+      <Images url='https://drive.google.com/file/d/1TZVYrB4YU0wEyr8bcArTF3wL9ntWIhkh/view?usp=sharing' alt="" />
       <Images url={Img2} alt="" />
       <Images url={Img3} alt="" />
       <Images url={Img4} alt="" />
