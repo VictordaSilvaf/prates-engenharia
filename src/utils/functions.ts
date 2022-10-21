32dbeffd61d8573d541439ce225726c825bddf05
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-10-21 15:19:50 -0300

    add lazyload

--- src/utils/functions.ts
+++
@@ -1,8 +0,0 @@
-export const importAll = (r: any) => {
- let images = [{}];
-  r.keys().forEach((item: any, index: any) => { 
-    images[item.replace('./', '')] = r(item); 
-  });
-
- return images
-}
--- src/utils/sliders.ts
+++
@@ -1,6 +0,0 @@
-export const portfolio = [
-  {
-    name: "COPAN - SALA2 ARQUITETURA",
-    
-  }
-]
