3b8fce37704db8135ed3d7ea96800914ec8e5c85
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-11-11 19:07:16 -0300

    Eager teste

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,19 +2,19 @@ import { Footer } from "../components/Footer";
 import { CustomSlider } from "../components/CustomSlider";
 
 export function Portfolio() {
-  const slider1 = import.meta.globEager("/public/portfolio/1AK-SALA2/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider2 = import.meta.globEager("/public/portfolio/2G_M-FelipeKilares/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider3 = import.meta.globEager("/public/portfolio/3B_R-SALA2/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider4 = import.meta.globEager("/public/portfolio/4LM-APTO41/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider5 = import.meta.globEager("/public/portfolio/5M_V-ISABELAFRAIA/*.(jpg|png|jpeg)", { as: 'raw' });
-  // const slider6 = import.meta.globEager("/public/portfolio/7JR-DUDASENNA/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider7 = import.meta.globEager("/public/portfolio/8A_C-FSTUDIO/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider8 = import.meta.globEager("/public/portfolio/9B_G-SALA2/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider9 = import.meta.globEager("/public/portfolio/10M_B/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider10 = import.meta.globEager("/public/portfolio/12L_G-FIGUEIREDO_FISCHER/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider11 = import.meta.globEager("/public/portfolio/13LABORATORIO_GUARULHOS/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider12 = import.meta.globEager("/public/portfolio/14WENTZ-FELIPE_HESS/*.(jpg|png|jpeg)", { as: 'raw' });
-  const slider13 = import.meta.globEager("/public/portfolio/15RESTAURANTE-ESTUDIO_PENHA/*.(jpg|png|jpeg)", { as: 'raw' });
+  const slider1 = import.meta.globEager("/public/portfolio/1AK-SALA2/*.(jpg|png|jpeg)");
+  const slider2 = import.meta.globEager("/public/portfolio/2G_M-FelipeKilares/*.(jpg|png|jpeg)");
+  const slider3 = import.meta.globEager("/public/portfolio/3B_R-SALA2/*.(jpg|png|jpeg)");
+  const slider4 = import.meta.globEager("/public/portfolio/4LM-APTO41/*.(jpg|png|jpeg)");
+  const slider5 = import.meta.globEager("/public/portfolio/5M_V-ISABELAFRAIA/*.(jpg|png|jpeg)");
+  // const slider6 = import.meta.globEager("/public/portfolio/7JR-DUDASENNA/*.(jpg|png|jpeg)");
+  const slider7 = import.meta.globEager("/public/portfolio/8A_C-FSTUDIO/*.(jpg|png|jpeg)");
+  const slider8 = import.meta.globEager("/public/portfolio/9B_G-SALA2/*.(jpg|png|jpeg)");
+  const slider9 = import.meta.globEager("/public/portfolio/10M_B/*.(jpg|png|jpeg)");
+  const slider10 = import.meta.globEager("/public/portfolio/12L_G-FIGUEIREDO_FISCHER/*.(jpg|png|jpeg)");
+  const slider11 = import.meta.globEager("/public/portfolio/13LABORATORIO_GUARULHOS/*.(jpg|png|jpeg)");
+  const slider12 = import.meta.globEager("/public/portfolio/14WENTZ-FELIPE_HESS/*.(jpg|png|jpeg)");
+  const slider13 = import.meta.globEager("/public/portfolio/15RESTAURANTE-ESTUDIO_PENHA/*.(jpg|png|jpeg)");
 
   return (
     <>
