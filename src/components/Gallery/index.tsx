704c5f38412afb4ba203c6db14f8d849718807cf
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-11-13 15:03:08 -0300

    tentativa de ajuste

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,15 +1,16 @@
 import { LazyBlock } from "../LazyBlock";
 
-const images = import.meta.glob("/src/assets/img/home/*.(jpg|png|jpeg)");
+import { HomeImages } from "../../assets/img/home";
 
 export function Gallery() {
-  const blockImages = Object.keys(images);
+  const images = Object.values(HomeImages[0]);
+  console.log(images);
 
   return (
     <div className="flex justify-center w-full">
       <div className="flex flex-wrap justify-center min-h-8">
-        {blockImages.map((image, index) => (
-          <LazyBlock url={image} key={index} />
+        {images.map((image) => (
+          <LazyBlock url={image} />
         ))}
       </div>
     </div>
