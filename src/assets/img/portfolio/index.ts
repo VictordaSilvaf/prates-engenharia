41231f5d7c8e758c47d896748f9f4058a5f337ef
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-11-13 16:19:50 -0300

    adc imgs portfolio

--- src/assets/img/portfolio/index.ts
+++ src/assets/img/portfolio/index.ts
@@ -11,6 +11,145 @@ import img2_3 from "./2G_M-FelipeKilares/5.png";
 import img2_4 from "./2G_M-FelipeKilares/6.png";
 import img2_5 from "./2G_M-FelipeKilares/9.png";
 import img2_6 from "./2G_M-FelipeKilares/10.png";
+import img2_7 from "./2G_M-FelipeKilares/11.png";
+import img2_8 from "./2G_M-FelipeKilares/13.png";
+import img2_9 from "./2G_M-FelipeKilares/14.png";
+import img2_10 from "./2G_M-FelipeKilares/16.png";
+import img2_11 from "./2G_M-FelipeKilares/18.png";
+import img2_12 from "./2G_M-FelipeKilares/19.png";
+import img2_13 from "./2G_M-FelipeKilares/27.png";
+import img2_14 from "./2G_M-FelipeKilares/28.png";
+
+// 3B_R-SALA2
+import img3_1 from "./3B_R-SALA2/1.png";
+import img3_2 from "./3B_R-SALA2/2.png";
+import img3_3 from "./3B_R-SALA2/3.png";
+import img3_6 from "./3B_R-SALA2/6.png";
+import img3_7 from "./3B_R-SALA2/7.png";
+import img3_8 from "./3B_R-SALA2/8.png";
+import img3_14 from "./3B_R-SALA2/14.png";
+import img3_15 from "./3B_R-SALA2/15.png";
+import img3_16 from "./3B_R-SALA2/16.png";
+import img3_19 from "./3B_R-SALA2/19.png";
+
+// 4LM-APTO41
+import img4_1 from "./4LM-APTO41/1.png";
+import img4_2 from "./4LM-APTO41/2.png";
+import img4_3 from "./4LM-APTO41/3.png";
+import img4_4 from "./4LM-APTO41/4.png";
+import img4_5 from "./4LM-APTO41/5.png";
+import img4_6 from "./4LM-APTO41/6.png";
+import img4_7 from "./4LM-APTO41/7.png";
+import img4_10 from "./4LM-APTO41/10.png";
+import img4_11 from "./4LM-APTO41/11.png";
+import img4_12 from "./4LM-APTO41/12.png";
+import img4_13 from "./4LM-APTO41/13.png";
+import img4_14 from "./4LM-APTO41/14.png";
+
+// 5M_V-ISABELAFRAIA
+import img5_1 from "./5M_V-ISABELAFRAIA/1.png";
+import img5_2 from "./5M_V-ISABELAFRAIA/2.png";
+import img5_3 from "./5M_V-ISABELAFRAIA/4.png";
+
+// 7JR-DUDASENNA
+import img6_1 from "./7JR-DUDASENNA/1.png";
+import img6_2 from "./7JR-DUDASENNA/2.png";
+import img6_3 from "./7JR-DUDASENNA/3.png";
+import img6_4 from "./7JR-DUDASENNA/4.png";
+import img6_5 from "./7JR-DUDASENNA/5.png";
+import img6_6 from "./7JR-DUDASENNA/6.png";
+import img6_7 from "./7JR-DUDASENNA/7.png";
+import img6_8 from "./7JR-DUDASENNA/8.png";
+import img6_9 from "./7JR-DUDASENNA/9.png";
+import img6_10 from "./7JR-DUDASENNA/10.png";
+import img6_11 from "./7JR-DUDASENNA/11.png";
+import img6_12 from "./7JR-DUDASENNA/12.png";
+import img6_13 from "./7JR-DUDASENNA/13.png";
+import img6_14 from "./7JR-DUDASENNA/14.png";
+import img6_15 from "./7JR-DUDASENNA/15.png";
+import img6_16 from "./7JR-DUDASENNA/16.png";
+import img6_17 from "./7JR-DUDASENNA/17.png";
+import img6_18 from "./7JR-DUDASENNA/18.png";
+import img6_19 from "./7JR-DUDASENNA/19.png";
+
+// 8A_C-FSTUDIO
+import img7_1 from "./7JR-DUDASENNA/1.png";
+import img7_2 from "./7JR-DUDASENNA/2.png";
+import img7_3 from "./7JR-DUDASENNA/3.png";
+
+// 9B_G-SALA2
+import img8_1 from "./9B_G-SALA2/1.png";
+import img8_2 from "./9B_G-SALA2/2.png";
+import img8_3 from "./9B_G-SALA2/6.png";
+import img8_4 from "./9B_G-SALA2/7.png";
+import img8_5 from "./9B_G-SALA2/8.png";
+import img8_6 from "./9B_G-SALA2/10.png";
+import img8_7 from "./9B_G-SALA2/11.png";
+import img8_8 from "./9B_G-SALA2/12.png";
+import img8_9 from "./9B_G-SALA2/14.png";
+import img8_10 from "./9B_G-SALA2/15.png";
+import img8_11 from "./9B_G-SALA2/16.png";
+
+// 10M_B
+// import img9_1 from "./10M_B/3.png";
+// import img9_2 from "./10M_B/4.png";
+// import img9_3 from "./10M_B/5.png";
+// import img9_4 from "./10M_B/6.png";
+
+// 12L_G-FIGUEIREDO_FISCHER
+import img10_1 from "./12L_G-FIGUEIREDO_FISCHER/1.png";
+import img10_2 from "./12L_G-FIGUEIREDO_FISCHER/4.png";
+import img10_3 from "./12L_G-FIGUEIREDO_FISCHER/5.png";
+import img10_4 from "./12L_G-FIGUEIREDO_FISCHER/10.png";
+import img10_5 from "./12L_G-FIGUEIREDO_FISCHER/11.png";
+import img10_6 from "./12L_G-FIGUEIREDO_FISCHER/12.png";
+import img10_7 from "./12L_G-FIGUEIREDO_FISCHER/14.png";
+import img10_8 from "./12L_G-FIGUEIREDO_FISCHER/19.png";
+import img10_9 from "./12L_G-FIGUEIREDO_FISCHER/20.png";
+import img10_10 from "./12L_G-FIGUEIREDO_FISCHER/21.png";
+import img10_11 from "./12L_G-FIGUEIREDO_FISCHER/22.png";
+import img10_12 from "./12L_G-FIGUEIREDO_FISCHER/23.png";
+import img10_13 from "./12L_G-FIGUEIREDO_FISCHER/25.png";
+import img10_14 from "./12L_G-FIGUEIREDO_FISCHER/26.png";
+import img10_15 from "./12L_G-FIGUEIREDO_FISCHER/27.png";
+
+// 13LABORATORIO_GUARULHOS
+import img11_1 from "./13LABORATORIO_GUARULHOS/1.png";
+import img11_2 from "./13LABORATORIO_GUARULHOS/2.png";
+import img11_3 from "./13LABORATORIO_GUARULHOS/3.png";
+import img11_4 from "./13LABORATORIO_GUARULHOS/4.png";
+import img11_5 from "./13LABORATORIO_GUARULHOS/5.png";
+import img11_6 from "./13LABORATORIO_GUARULHOS/6.png";
+import img11_7 from "./13LABORATORIO_GUARULHOS/7.png";
+import img11_8 from "./13LABORATORIO_GUARULHOS/8.png";
+import img11_9 from "./13LABORATORIO_GUARULHOS/9.png";
+import img11_10 from "./13LABORATORIO_GUARULHOS/10.png";
+
+// 14WENTZ-FELIPE_HESS
+import img12_1 from "./14WENTZ-FELIPE_HESS/1.png";
+import img12_2 from "./14WENTZ-FELIPE_HESS/2.png";
+import img12_3 from "./14WENTZ-FELIPE_HESS/3.png";
+import img12_4 from "./14WENTZ-FELIPE_HESS/4.png";
+import img12_5 from "./14WENTZ-FELIPE_HESS/5.png";
+import img12_6 from "./14WENTZ-FELIPE_HESS/6.png";
+import img12_7 from "./14WENTZ-FELIPE_HESS/7.png";
+import img12_8 from "./14WENTZ-FELIPE_HESS/8.png";
+import img12_9 from "./14WENTZ-FELIPE_HESS/9.png";
+import img12_10 from "./14WENTZ-FELIPE_HESS/10.png";
+import img12_11 from "./14WENTZ-FELIPE_HESS/11.png";
+
+// 15RESTAURANTE-ESTUDIO_PENHA
+import img13_1 from "./15RESTAURANTE-ESTUDIO_PENHA/1.png";
+import img13_2 from "./15RESTAURANTE-ESTUDIO_PENHA/2.png";
+import img13_3 from "./15RESTAURANTE-ESTUDIO_PENHA/3.png";
+import img13_4 from "./15RESTAURANTE-ESTUDIO_PENHA/4.png";
+import img13_5 from "./15RESTAURANTE-ESTUDIO_PENHA/5.png";
+import img13_6 from "./15RESTAURANTE-ESTUDIO_PENHA/7.png";
+import img13_7 from "./15RESTAURANTE-ESTUDIO_PENHA/8.png";
+import img13_8 from "./15RESTAURANTE-ESTUDIO_PENHA/9.png";
+import img13_9 from "./15RESTAURANTE-ESTUDIO_PENHA/10.png";
+import img13_10 from "./15RESTAURANTE-ESTUDIO_PENHA/11.png";
+import img13_11 from "./15RESTAURANTE-ESTUDIO_PENHA/12.png";
 
 export const PortfolioImgs = [
   {
@@ -19,6 +158,172 @@ export const PortfolioImgs = [
   },
   {
     title: "G_M-FelipeKilares",
-    images: { img2_1, img2_2, img2_3, img2_4, img2_5, img2_6 },
+    images: {
+      img2_1,
+      img2_2,
+      img2_3,
+      img2_4,
+      img2_5,
+      img2_6,
+      img2_7,
+      img2_8,
+      img2_9,
+      img2_10,
+      img2_11,
+      img2_12,
+      img2_13,
+      img2_14,
+    },
+  },
+  {
+    title: "B&R - SALA2 ARQUITETURA",
+    images: {
+      img3_1,
+      img3_2,
+      img3_3,
+      img3_6,
+      img3_7,
+      img3_8,
+      img3_14,
+      img3_15,
+      img3_16,
+      img3_19,
+    },
+  },
+  {
+    title: "LM - APTO41 ARQUITETURA",
+    images: {
+      img4_1,
+      img4_2,
+      img4_3,
+      img4_4,
+      img4_5,
+      img4_6,
+      img4_7,
+      img4_10,
+      img4_11,
+      img4_12,
+      img4_13,
+      img4_14,
+    },
+  },
+  {
+    title: "CLÍNICA - ISABELA FRAIA",
+    images: { img5_1, img5_2, img5_3 },
+  },
+  {
+    title: "JR - DUDA SENNA ARQUITETURA",
+    images: {
+      img6_1,
+      img6_2,
+      img6_3,
+      img6_4,
+      img6_5,
+      img6_6,
+      img6_7,
+      img6_8,
+      img6_9,
+      img6_10,
+      img6_11,
+      img6_12,
+      img6_13,
+      img6_14,
+      img6_15,
+      img6_16,
+      img6_17,
+      img6_18,
+      img6_19,
+    },
+  },
+  {
+    title: "ANAS - F.STUDIO ARQUITETURA",
+    images: {
+      img7_1,
+      img7_2,
+      img7_3,
+    },
+  },
+  {
+    title: "B&G - SALA2 ARQUITETURA",
+    images: {
+      img8_1,
+      img8_2,
+      img8_3,
+      img8_4,
+      img8_5,
+      img8_6,
+      img8_7,
+      img8_8,
+      img8_9,
+      img8_10,
+      img8_11,
+    },
+  },
+  {
+    title: "GHI - FIGUEIREDO FISCHER ARQUITETOS",
+    images: {
+      img10_1,
+      img10_2,
+      img10_3,
+      img10_4,
+      img10_5,
+      img10_6,
+      img10_7,
+      img10_8,
+      img10_9,
+      img10_10,
+      img10_11,
+      img10_12,
+      img10_13,
+      img10_14,
+      img10_15,
+    },
+  },
+  {
+    title: "LABGRU - PIETRO TERLIZZI ARQUITETURA",
+    images: {
+      img11_1,
+      img11_2,
+      img11_3,
+      img11_4,
+      img11_5,
+      img11_6,
+      img11_7,
+      img11_8,
+      img11_9,
+      img11_10,
+    },
+  },
+  {
+    title: "WENTZ - FELIPE HESS ARQUITETOS",
+    images: {
+      img12_1,
+      img12_2,
+      img12_3,
+      img12_4,
+      img12_5,
+      img12_6,
+      img12_7,
+      img12_8,
+      img12_9,
+      img12_10,
+      img12_11,
+    },
+  },
+  {
+    title: "JC RESTAURANTE - ESTÚDIO PENHA ARQUITETOS",
+    images: {
+      img13_1,
+      img13_2,
+      img13_3,
+      img13_4,
+      img13_5,
+      img13_6,
+      img13_7,
+      img13_8,
+      img13_9,
+      img13_10,
+      img13_11,
+    },
   },
 ];
